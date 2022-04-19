e1490e1bf09065b641d1699d599fa0a639458183
Simon Giller <simon.giller@sympany.ch>
2022-04-19 16:50:43 +0200

    Delete not needed quarkus IT

--- quarkus-extension/integration-tests/src/test/java/ch/sympany/quarkus/vaadin/extension/it/NativeQuarkusVaadinExtensionResourceIT.java
+++
@@ -1,7 +0,0 @@
-package ch.sympany.quarkus.vaadin.extension.it;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeQuarkusVaadinExtensionResourceIT extends QuarkusVaadinExtensionResourceTest {
-}
